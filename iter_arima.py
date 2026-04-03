2c757563e1992edee76a6878bb8a1aa503b91b4b
uvp <uuuuuvp@gmail.com>
2026-04-03 16:56:01 +0800

    "arima 添加 Yeo-Johnson 变换"

--- iter_arima.py
+++ iter_arima.py
@@ -3,10 +3,8 @@ import pandas as pd
 import numpy as np
 from pmdarima import auto_arima
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import PowerTransformer
 import warnings
-
-# 从你的 utils 中引用 Mmape 和 read_data_from_csv
-# 假设你的文件名是 utils.py，如果不是请修改下行
 from utils import Mmape, read_data_from_csv 
 
 warnings.filterwarnings("ignore")
@@ -17,11 +15,12 @@ input_file = "E:\\code\\mission\\full_sampled_lines.csv"
 column_name = 'I_P'  # 保持与 LSTM 实验一致
 train_points = 800
 forecast_horizon = 12
-sigma_threshold = 3
+Otl_Plt_M = 'sigma'
+threshold = 3
 
 # ARIMA 参数范围（bic 通常比 aic 更倾向于简单模型，防止过拟合）
 criterion = 'bic' 
-output_file = f"arima_vs_lstm_bench_{column_name}.csv"
+output_file = f"arima-{criterion}I{train_points}F{forecast_horizon}{column_name}.csv"
 
 # ================== 读取数据与初始化 ==================
 lines_df = pd.read_csv(input_file)
@@ -49,13 +48,16 @@ for idx, row in results_df.iterrows():
             column_name=column_name,
             train_points=train_points,
             forecast_horizon=forecast_horizon,
-            sigma_threshold=sigma_threshold
+            sigma_threshold=threshold
         )
+        
+        pt = PowerTransformer(method='yeo-johnson')
+        train_transformed = pt.fit_transform(train.reshape(-1, 1)).flatten()
         
         # 2. 自动寻找最优 ARIMA 模型
         # m=12 代表季节性周期（一天12个点，每2小时一个点）
         model = auto_arima(
-            train,
+            train_transformed,
             seasonal=True, m=12,
             information_criterion=criterion,
             suppress_warnings=True,
@@ -64,14 +66,16 @@ for idx, row in results_df.iterrows():
         )
         
         # 3. 预测未来 12 个点
-        y_pred = model.predict(n_periods=forecast_horizon)
+        # y_pred = model.predict(n_periods=forecast_horizon)
+        # 预测后必须反变换回来
+        y_pred_transformed = model.predict(n_periods=forecast_horizon)
+        y_pred = pt.inverse_transform(y_pred_transformed.reshape(-1, 1)).flatten()
         y_true = test
         
         # 4. 调用统一指标
         mse_l = mean_squared_error(y_true, y_pred)
         mae_l = mean_absolute_error(y_true, y_pred)
         r2_l = r2_score(y_true, y_pred)
-        # 调用你 utils 里的 Mmape
         mape_l = Mmape(y_true, y_pred, threshold_ratio=0.05)
         
         # 5. 记录参数和结果
@@ -90,4 +94,4 @@ for idx, row in results_df.iterrows():
         results_df.to_csv(output_file, index=False, encoding='utf-8-sig')
 
 results_df.to_csv(output_file, index=False, encoding='utf-8-sig')
-print("\nARIMA 对标实验结束。")
+print("\nARIMA 结果已保存到:", output_file)
